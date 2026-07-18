30c49769d7b59f56e610b5f4e1f21bdb2fac3ebc
Ernesto Solano <solanoed@uninorte.edu.co>
2026-07-18 17:40:18 -0500

    test: register AttachmentExtractor to isolate whether playback bug is platform-specific

--- backend/src/bot/createPlayer.ts
+++ backend/src/bot/createPlayer.ts
@@ -1,9 +1,10 @@
 import { Client } from 'discord.js';
 import { Player } from 'discord-player';
-import { SoundCloudExtractor } from '@discord-player/extractor';
+import { AttachmentExtractor, SoundCloudExtractor } from '@discord-player/extractor';
 
 export async function createPlayer(client: Client): Promise<Player> {
   const player = new Player(client, { skipFFmpeg: false });
   await player.extractors.register(SoundCloudExtractor, {});
+  await player.extractors.register(AttachmentExtractor, {});
   return player;
 }
